aad3129664019b7e7026bcbb6dbc537a0c3039b4
Dustin Sallings <dustin@spy.net>
2008-04-18 14:20:17 -0700

    New packet formats as described in our fourth hackathon.

--- memcacheConstants.py
+++ memcacheConstants.py
@@ -20,11 +20,11 @@ CMD_GETQ = 9
 CMD_NOOP = 10
 CMD_VERSION = 11
 
-# cas ID, Flags, expiration
-SET_PKT_FMT=">QII"
+# Flags, expiration
+SET_PKT_FMT=">II"
 
-# cas ID, flags
-GET_RES_FMT=">QI"
+# flags
+GET_RES_FMT=">I"
 
 # How long until the deletion takes effect.
 DEL_PKT_FMT=">I"
@@ -39,12 +39,14 @@ MAGIC_BYTE = 0x80
 REQ_MAGIC_BYTE = 0x80
 RES_MAGIC_BYTE = 0x81
 
-# magic, opcode, keylen, extralen, datatype, [reserved], bodylen, opaque
-REQ_PKT_FMT=">BBHBBxxII"
-# magic, opcode, status, extralen, datatype, [reserved], bodylen, opaque
-RES_PKT_FMT=">BBHBBxxII"
+# magic, opcode, keylen, extralen, datatype, [reserved], bodylen, opaque, cas
+REQ_PKT_FMT=">BBHBBxxIIQ"
+# magic, opcode, keylen, extralen, datatype, status, bodylen, opaque, cas
+RES_PKT_FMT=">BBHBBHIIQ"
 # min recv packet size
 MIN_RECV_PACKET = struct.calcsize(REQ_PKT_FMT)
+# The header sizes don't deviate
+assert struct.calcsize(REQ_PKT_FMT) == struct.calcsize(RES_PKT_FMT)
 
 EXTRA_HDR_FMTS={
     CMD_SET: SET_PKT_FMT,
